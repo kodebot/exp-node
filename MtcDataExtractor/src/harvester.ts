40b756274d05f341e824ed83b8e5eef870f6b269
Manikandan Venkatraman <vmanikandan001@hotmail.com>
2015-09-29 21:06:32 +0100

    added function to get route info for each rotue and modified to handle promises better

--- MtcDataExtractor/src/harvester.ts
+++ MtcDataExtractor/src/harvester.ts
@@ -4,12 +4,15 @@ import webdriver = require('selenium-webdriver');
 import By = webdriver.By;
 import until = webdriver.until;
 import _ = require("lodash");
+import q = require("q");
+import types = require("types");
 
 
 const MTC_MAIN_URL: string = "http://www.mtcbus.org/";
 const MTC_ROUTES_URL: string = "http://mtcbus.org/Routes.asp";
 const MTC_STAGES_URL: string = "http://mtcbus.org/Places.asp";
 const MTC_ROUTES_SELECT_ELEM: string = "cboRouteCode";
+const MTC_ROUTE_INFO_TABLE_XPATH: string = "/html/body/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[7]/td/table/tbody/";
 export class Harvester {
 	constructor(private driver: webdriver.WebDriver) {
 	}
@@ -50,12 +53,18 @@ export class Harvester {
 			this.driver.get(MTC_ROUTES_URL);
 
 			this.getRoutes((err, options) => {
+				var actions: any[] = [];
+
 				options.forEach((option, index) => {
-					this.driver.findElements(By.tagName("option"))
+					actions.push(this.driver.findElements(By.tagName("option"))
 						.then(elems => elems[index].click())
 						.then(_ => this.driver.findElement(By.name("submit")))
-						.then(elem => elem.click());
+						.then(elem => elem.click())
+						.then(() => this.getRouteInfo(this.driver)));
 				});
+
+				return q.all(actions).then(data => console.log(data));
+
 			});
 
 
@@ -63,6 +72,38 @@ export class Harvester {
 		} catch (e) {
 			return cb(e, null);
 		}
+	}
+
+	private getRouteInfo(driver: webdriver.WebDriver) {
+		let routeDetailXpath = MTC_ROUTE_INFO_TABLE_XPATH + "tr[3]/td";
+
+		let routeInfo: types.RouteInfo = {
+			routeNo: "", serviceType: "", origin: "", destination: "", journeyTime: 0, stages: []
+		};
+		var pr1 = driver.findElements(By.xpath(routeDetailXpath))
+			.then(elems => {
+				return q.all<any>([elems[0].getText().then(text=> routeInfo.routeNo = text),
+					elems[1].getText().then(text=> routeInfo.serviceType = text),
+					elems[2].getText().then(text=> routeInfo.origin = text),
+					elems[3].getText().then(text=> routeInfo.destination = text),
+					elems[4].getText().then(text=> routeInfo.journeyTime = parseInt(text))
+				]);
+			});
+
+		var pr2 = driver.findElements(By.xpath(MTC_ROUTE_INFO_TABLE_XPATH + "tr"))
+			.then(elems => {
+				var actions = elems.map((elem, index) => {
+					if (index > 4 && index < elems.length - 2) {
+						return elems[index].findElements(By.tagName("td"))
+							.then(tds => tds[1].getText().then(text => routeInfo.stages.push(text)));
+					}
+				});
+
+				return q.all(actions);
+			});
+
+		return q.all<any>([pr1, pr2])
+			.then(_ => routeInfo);
 	}
 
 	private getStagesOfARoute(text: string, cb: (err: Error, states: { [index: string]: string[] }) => void) {
---
+++ MtcDataExtractor/src/types.ts
@@ -0,0 +1,8 @@
+export interface RouteInfo {
+	routeNo: string;
+	serviceType: string;
+	origin: string;
+	destination: string;
+	journeyTime: number;
+	stages:string[];
+}
